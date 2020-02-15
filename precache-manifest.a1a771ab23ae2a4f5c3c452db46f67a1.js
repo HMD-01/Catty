d9a6e9e62a088023aace502b2e37c615227d4495
HMD-01 <ahmedolajimoh@gmail.com>
2020-02-15 13:59:39 +0100

    Updates

---
+++ precache-manifest.a1a771ab23ae2a4f5c3c452db46f67a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42c113a7fc922a9c5a657e5f61efb456",
+    "url": "/Catty/index.html"
+  },
+  {
+    "revision": "37ae1696dcf64a58d9f6",
+    "url": "/Catty/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "34aa428999773444a432",
+    "url": "/Catty/static/css/main.3832e726.chunk.css"
+  },
+  {
+    "revision": "37ae1696dcf64a58d9f6",
+    "url": "/Catty/static/js/2.ed51ba71.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Catty/static/js/2.ed51ba71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34aa428999773444a432",
+    "url": "/Catty/static/js/main.98dcc6de.chunk.js"
+  },
+  {
+    "revision": "bf9319667ef7d4759f08",
+    "url": "/Catty/static/js/runtime-main.b32f55a3.js"
+  },
+  {
+    "revision": "680d0347117be7b57403ea181aa5d33c",
+    "url": "/Catty/static/media/animatedLoader.680d0347.gif"
+  }
+]);
